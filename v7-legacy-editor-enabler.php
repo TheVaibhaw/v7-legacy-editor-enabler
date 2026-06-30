21c4b9599841f65cddcf1d049837cc2653238974
Vaibhaw Kumar <imvaibhaw@gmail.com>
2026-06-30 17:56:09 +0530

    feat: Add dynamic post type checkboxes for legacy editor control
    - Automatically detect and display all public post types in settings
    - Allow users to enable/disable legacy editor per post type via checkboxes
    - All post types checked by default
    - Bump version to 1.2.0

--- v7-legacy-editor-enabler.php
+++ v7-legacy-editor-enabler.php
@@ -3,7 +3,7 @@
  * Plugin Name:       V7 Legacy Editor Enabler
  * Plugin URI:        https://github.com/TheVaibhaw/v7-legacy-editor-enabler
  * Description:       A lightweight plugin to disable Gutenberg block editor and enable the legacy TinyMCE editor for Posts and Pages with granular control.
- * Version:           1.1.0
+ * Version:           1.2.0
  * Author:            Vaibhaw Kumar
  * Author URI:        https://vaibhawkumar.in
  * License:           GPL-2.0-or-later
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'V7_LEGACY_EDITOR_ENABLER_VERSION', '1.0.0' );
+define( 'V7_LEGACY_EDITOR_ENABLER_VERSION', '1.2.0' );
 
 // phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound
 /**
